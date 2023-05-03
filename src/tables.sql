4eaedc82c036aa25e92d83ef5c251841944f9aa3
Raúl Rueda <74800303+xzwjz2@users.noreply.github.com>
2023-05-03 17:02:55 -0300

    Update tables.sql

--- src/tables.sql
+++ src/tables.sql
@@ -7,49 +7,49 @@ SET foreign_key_checks = 0;
 SET NAMES utf8mb4;
 
 CREATE TABLE `mid` (
-  `msisdn` varchar(20) NOT NULL COMMENT 'Nro de telefono',
-  `userid` bigint(20) NOT NULL COMMENT 'Id del usuario',
-  `chatid` bigint(20) NOT NULL COMMENT 'Id del chat',
-  `verif` tinyint(3) unsigned NOT NULL COMMENT 'Verificado',
-  `fchver` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Fecha de verificación',
+  `msisdn` varchar(20) NOT NULL COMMENT 'Phone Number',
+  `userid` bigint(20) NOT NULL COMMENT 'User ID',
+  `chatid` bigint(20) NOT NULL COMMENT 'Chat ID',
+  `verif` tinyint(3) unsigned NOT NULL COMMENT '0: number is not verified yet, 1: in process of verication, 2: verified, 3: received error 429 should be verified again in the future',
+  `fchver` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Verification date',
   PRIMARY KEY (`msisdn`),
   KEY `userid` (`userid`),
   KEY `verif` (`verif`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla de ChatsId de los números';
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Table with phone numbers and IDs';
 
 
 CREATE TABLE `mo` (
-  `idmo` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Id del registro',
-  `userid` bigint(20) NOT NULL COMMENT 'Id del usuario de origen',
-  `chatid` bigint(20) NOT NULL COMMENT 'Id del chat',
-  `msgid` bigint(20) NOT NULL COMMENT 'Id del mensaje',
-  `fchrec` datetime NOT NULL COMMENT 'Fecha y hora de recibido',
-  `mensaje` varchar(8000) NOT NULL COMMENT 'Texto del Mensaje',
-  `estado` tinyint(3) unsigned NOT NULL COMMENT 'Estado del mensaje',
+  `idmo` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Record ID',
+  `userid` bigint(20) NOT NULL COMMENT 'User ID of sender',
+  `chatid` bigint(20) NOT NULL COMMENT 'Chat ID of sender',
+  `msgid` bigint(20) NOT NULL COMMENT 'Message ID',
+  `fchrec` datetime NOT NULL COMMENT 'Datetime received',
+  `mensaje` varchar(8000) NOT NULL COMMENT 'Text of message',
+  `estado` tinyint(3) unsigned NOT NULL COMMENT 'Status of message',
   PRIMARY KEY (`idmo`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla de mensajes recibidos';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Table of received messages';
 
 
 CREATE TABLE `mt` (
-  `idmt` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Id del registro',
-  `origen` varchar(20) NOT NULL COMMENT 'Identificador del origen',
-  `msisdn` varchar(20) NOT NULL COMMENT 'Nro de telefono de destino',
-  `mensaje` varchar(2000) NOT NULL COMMENT 'Texto del mensaje',
-  `fching` datetime NOT NULL COMMENT 'Fecha de ingreso',
-  `fchpro` datetime NOT NULL COMMENT 'Fecha de proceso',
-  `fchenv` datetime NOT NULL COMMENT 'Fecha de envío',
-  `fchent` datetime NOT NULL COMMENT 'Fecha de entrega',
-  `fchlei` datetime NOT NULL COMMENT 'Fecha de lectura',
-  `estado` tinyint(3) unsigned NOT NULL COMMENT 'Estado: 0-No enviado, 1-Pendiente, 2-Error en envio, 3-Enviado, 4-Entregado, 5-Leído',
-  `coderr` varchar(100) NOT NULL COMMENT 'Código del error',
-  `userid` bigint(20) NOT NULL COMMENT 'Id del usuario',
-  `chatid` bigint(20) NOT NULL COMMENT 'Id del chat',
-  `msgid` bigint(20) NOT NULL COMMENT 'Id asignado al enviar',
-  `borrado` tinyint(3) unsigned NOT NULL COMMENT 'Código de borrado: 0: activo, 1: borrado',
+  `idmt` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Record ID',
+  `origen` varchar(20) NOT NULL COMMENT 'Origin (put anything you want, name of app that generate the message for example)',
+  `msisdn` varchar(20) NOT NULL COMMENT 'Phone number of destiny',
+  `mensaje` varchar(2000) NOT NULL COMMENT 'Text of message',
+  `fching` datetime NOT NULL COMMENT 'Datetime record added',
+  `fchpro` datetime NOT NULL COMMENT 'Datetime processed',
+  `fchenv` datetime NOT NULL COMMENT 'Datetime sent',
+  `fchent` datetime NOT NULL COMMENT 'Datetime delivered',
+  `fchlei` datetime NOT NULL COMMENT 'Datetime readed',
+  `estado` tinyint(3) unsigned NOT NULL COMMENT 'Status: 0-Unsent, 1-Pending, 2-Error.Not sent, 3-Sent, 4-Delivered, 5-Readed',
+  `coderr` varchar(100) NOT NULL COMMENT 'Description of error (for status 2)',
+  `userid` bigint(20) NOT NULL COMMENT 'User ID',
+  `chatid` bigint(20) NOT NULL COMMENT 'Chat ID',
+  `msgid` bigint(20) NOT NULL COMMENT 'Message ID',
+  `borrado` tinyint(3) unsigned NOT NULL COMMENT '(not used)',
   PRIMARY KEY (`idmt`),
   KEY `chatid_idenvio` (`chatid`,`msgid`),
   KEY `estado` (`estado`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla de mensajes enviados';
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Table of sent messages';
 
 
 -- 2023-05-03 13:36:49
